3804a9fa0616e18c3f5c71ff143243dfb8801374
Jeremy0100 <jeremy.strady@mailfence.com>
2024-06-18 11:17:41 +0000

    modified:   6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,12 +2,11 @@
 
 int main (void)
 {
-  printf(sizeof(char));
-  printf(sizeof(int));
-  printf(sizeof(long long int));
-  printf(sizeof(long int));
-  printf(sizeof(float));
-
+  printf("Size",sizeof(char));
+  printf("Size",sizeof(int));
+  printf("Size",sizeof(long long int));
+  printf("Size",sizeof(long int));
+  printf("Size",sizeof(float));
 }
 
 return (0);
