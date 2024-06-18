65d9d686839f2035f49f2a88fb75e8660c2752c5
Jeremy0100 <jeremy.strady@mailfence.com>
2024-06-18 09:51:48 +0000

    new file:   6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void)
+{
+  printf(sizeof(char));
+  printf(sizeof(int));
+  printf(sizeof(long long int));
+  printf(sizeof(long int));
+  printf(sizeof(float));
+
+}
+
+return (0);
