562d6a4f1f0ffddcb1403b178bd55ec7b46e91e4
Jeremy0100 <jeremy.strady@mailfence.com>
2024-06-19 08:40:01 +0000

    new file:   5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+
+/**include**/
+#include <stdio.h>
+
+/**main**/
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void) /**main**/
+{
+	int num;
+	/** do stuff **/
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
