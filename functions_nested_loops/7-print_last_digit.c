3970d1db6764d2583737b8f098a10fa9b12b57ea
Jeremy0100 <jeremy.strady@mailfence.com>
2024-06-20 13:15:00 +0000

    new file:   7-print_last_digit.c 	modified:   main.h

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - begining
+ *
+ * Description: program
+ *
+ * @c: is input
+ *
+ * Return: 0 ends the program
+ */
+
+int print_last_digit(int c)
+{
+int last;
+
+last = c % 10;
+if (last < 0)
+last = last * -1;
+_putchar('0' + last);
+return (last);
