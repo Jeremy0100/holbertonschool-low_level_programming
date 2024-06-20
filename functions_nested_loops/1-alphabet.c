96bdfb64758f311ceaab87525ef4e4427139b12b
Jeremy0100 <jeremy.strady@mailfence.com>
2024-06-20 08:03:37 +0000

    modified:   1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,21 +1,5 @@
 #include "main.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- *
- *int main(void)
- *{
- *print_alphabet();
- *return (0);
- *}
-
-#include "main.h"
-/**
-* print_alphabet - Entry point
-* Return: Always 0 (Success)
-*/
 
 void print_alphabet(void)
 {
