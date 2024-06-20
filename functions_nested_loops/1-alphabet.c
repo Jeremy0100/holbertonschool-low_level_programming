2dc887cb067080d980ac6e96963f2632c6621214
Jeremy0100 <jeremy.strady@mailfence.com>
2024-06-20 08:00:53 +0000

    modified:   1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -16,14 +16,12 @@
 * print_alphabet - Entry point
 * Return: Always 0 (Success)
 */
+
 void print_alphabet(void)
 {
+	char letter;
 
-char alphabet;
-for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
-{
-
-_putchar(alphabet);
-}
-_putchar('\n');
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+		_putchar(letter);
+	_putchar('\n');
 }
