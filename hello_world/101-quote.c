ad0bc15bd8eacfd437ada755e39e39b359e09d83
Jeremy0100 <jeremy.strady@mailfence.com>
2024-10-22 08:45:03 +0000

    modified:   101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,2 +1,17 @@
-#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - Entry point
+*
+* Description: 'prints exactly [...],followed by a new line,standard error'
+*
+* Return: Always 1
+*
+*/
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
 
