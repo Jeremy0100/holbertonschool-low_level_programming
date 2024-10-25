763af8a5763592d159a712872b8e10cc1f599562
Jeremy0100 <jeremy.strady@mailfence.com>
2024-10-25 13:23:57 +0000

    modified:   8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,12 +13,13 @@ void jack_bauer(void)
 {
 	char h1, h2, m1, m2;
 
-	for (h1 = '0' ; (h1 <= '2') ; h1++)
+	for (h1 = '0' ; h1 <= '2'; h1++)
 	{
 		for (h2 = '0' ; ((h1 < '2' && h2 <= '9') || (h1 == '2' && h2 <= '3')); h2++)
 		{
-			for (m1 = '0' ; m1 <= '5' ; m1++)
-				for (m2 = '0' ; m1 <= '9' ; m2++)
+			for (m1 = '0' ; m1 <= '5'; m1++)
+			{
+				for (m2 = '0' ; m2 <= '9'; m2++)
 				{
 					_putchar(h1);
 					_putchar(h2);
@@ -26,6 +27,7 @@ void jack_bauer(void)
 					_putchar(m1);
 					_putchar(m2);
 					_putchar('\n');
+				}
 			}
 		}
 	}
