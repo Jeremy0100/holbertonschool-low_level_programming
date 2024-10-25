57682b1a2b90d77c734880e3ab1087217798af43
Jeremy0100 <jeremy.strady@mailfence.com>
2024-10-25 09:21:10 +0000

    new file:   6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _abs - computes absolute value of an interger
+*
+* Description: Program with a prototype
+*
+* @n: n variable
+*
+* Return: Nothing
+*
+*/
+
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (-n);
+}
