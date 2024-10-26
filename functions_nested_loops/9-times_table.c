c16d29f353b97b029067d3b4721da5f2c5acdad0
Jeremy0100 <jeremy.strady@mailfence.com>
2024-10-26 10:16:19 +0000

    modified:   9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,15 +14,26 @@
 
 void times_table(void)
 {
-	int i, j;
+	int num, mult, prod;
 
-	for (i = 0 ; i <= 9 ; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		printf("%d, ", i * 0);
-		for (j = 1 ; j <= 8 ; j++)
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
 		{
-			printf("%2d, ", i * j);
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
 		}
-		printf("%2d\n", i * j);
+		_putchar('\n');
 	}
 }
