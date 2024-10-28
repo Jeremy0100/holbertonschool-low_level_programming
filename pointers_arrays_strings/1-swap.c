84d28b4eeaa7887210e4100aa6b25fb452e79cd9
root <jeremy.strady@mailfence.com>
2024-10-28 15:37:03 +0000

    new file:   1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - swaps the values of two integers.
+*
+* Description: Swaps the values of two integers.
+*
+* @a @b: Starting
+*
+* Return: Always 0
+*
+*/
+
+
+void swap_int(int *a, int *b);
