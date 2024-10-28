a2303c8a20d34f4cff6e352cd9098f05fd37ee9d
root <jeremy.strady@mailfence.com>
2024-10-28 09:14:35 +0000

    new file:   2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* mul - multiplies two integers
+*
+* Description: multiplies two integers
+*
+* @a , @b: Starting
+*
+* Return: Nothing
+*
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
