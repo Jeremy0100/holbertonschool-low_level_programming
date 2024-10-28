ef5fd2c539d2df8dbc605d0a9d20647d1417ff9e
root <jeremy.strady@mailfence.com>
2024-10-28 14:11:34 +0000

    new file:   0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* reset_to_98 - takes a pointer to an int as parameter
+*
+* Description: takes pointer to an in and updates the value to 98
+*
+* @n: Starting number
+*
+* Return: Always 0
+*
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
