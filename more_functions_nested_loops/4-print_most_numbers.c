2a27925773640328fb88c3b775193254e9c8b03d
root <jeremy.strady@mailfence.com>
2024-10-28 09:49:41 +0000

    new file:   4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the numbers from 0 to 9
+*
+* Description: Prints the numbers from 0 to 9
+*
+* Return: Always 0
+*
+*/
+
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
