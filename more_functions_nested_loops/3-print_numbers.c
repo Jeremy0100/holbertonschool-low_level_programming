136b74696a7a438bd11313170b8751e0100509c4
root <jeremy.strady@mailfence.com>
2024-10-28 09:41:50 +0000

    new file:   3-print_numbers.c prints the numbers from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _putchar - prints the numbers from 0 to 9
+*
+* Description: Prints the numbers from 0 to 9 followed by new line
+*
+* Return: Always 0
+*
+*/
+
+int main(void)
+{
+    print_numbers();
+    return (0);
+}
