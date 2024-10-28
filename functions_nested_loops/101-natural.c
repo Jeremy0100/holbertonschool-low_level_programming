c7866bd8800c2c8365ad17f450e21ad201849df2
root <jeremy.strady@mailfence.com>
2024-10-28 15:26:08 +0000

    modified:   101-natural.c

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
+/**
+* main - list natural num. belows 10 that are multiples of 3 or 5.
+*
+* Description: list natural num. belows 10 that are multiples of 3 or 5.
+*
+* @a @b: Starting
+*
+* Return: Always 0
+*
+*/
+
 int main(void)
 {
 	int a, b;
-	for (a = 1; a < 1024; a++)
-		{
-		if ((a % 3) == 0 || (a % 5) == 0)
 
-		b += a;
+	for (a = 1; a < 1024; a++);
+		{
+		if ((a % 3) == 0 || (a % 5) == 0);
+		b = a;
 		}
 	printf("%d\n", b);
 	return (0);
