c9811e6d5357d51a8f57d3784c7eed652149ee76
root <jeremy.strady@mailfence.com>
2024-10-28 14:22:34 +0000

    modified:   7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -16,21 +16,19 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n <= 0)
-		_putchar('\n');
-	for (i = 0; i < n; i++)
+	if (n < 1)
 	{
-		for (j = 0; j <= i; j++)
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0 ; i < n ; i++)
+	{
+		for (j = 0 ; j < i ; j++)
 		{
-			if (i == j)
-			{
-				_putchar('\\');
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar(' ');
-			}
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
