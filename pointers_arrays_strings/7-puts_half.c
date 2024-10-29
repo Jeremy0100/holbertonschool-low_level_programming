26c04f9073f4dd9e732837e8a0044ebb575d5b23
Jeremy0100 <jeremy.strady@mailfence.com>
2024-10-29 13:26:11 +0000

    new file:   7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* puts_half - prints half of a string
+*
+* Description: Prints half of a string followed by a new line
+*
+* @str: Starting
+*
+* Return: Always 0
+*
+*/
+
+void puts_half(char *str)
+{
+	int i;
+	int halflen;
+	int len = strlen(str);
+
+	if (len % 2 == 0)
+	{
+		halflen = len / 2;
+	}
+	else
+	{
+		halflen = (len + 1) / 2;
+	}
+
+	for (i = halflen; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
