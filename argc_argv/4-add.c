6e68fa3f2406a6f59519706c33b76cdc58feb866
Jeremy0100 <jeremy.strady@mailfence.com>
2024-11-08 19:20:37 +0100

    nouveau fichier : 4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point of the program
+ * @argc: number of command-line arguments
+ * @argv: array
+ *
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	int d = 0;
+	int i, j;
+	char *c;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (c = argv[i]; *c; c++)
+		{
+			if (*c < '0' || *c > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		j = atoi(argv[i]);
+		if (j <= 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		d += j;
+	}
+
+	printf("%d\n", d);
+	return (0);
+}
