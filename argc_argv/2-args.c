319266145eb32667325407661c335b7be456049c
Jeremy0100 <jeremy.strady@mailfence.com>
2024-11-08 19:14:27 +0100

    nouveau fichier : 2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ * @argc: number of command-line arguments
+ * @argv: array
+ *
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
