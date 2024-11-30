fface82b89931e08afa5c6b052d47af63cbe90b6
Jeremy0100 <jeremy.strady@mailfence.com>
2024-11-30 21:49:42 +0000

    new file:   2-str_concat.c

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include "main.h"
+/**
+ * str_concat - concating 2 strings
+ * @s1: string
+ * @s2: string
+ * Return: pointer to new string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *arr, *p;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	arr = (char *)malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+	p = arr;
+	if (arr != NULL)
+	{
+		while (*s1)
+		{
+			*arr++ = *s1++;
+		}
+		while (*s2)
+		{
+			*arr++ = *s2++;
+		}
+	}
+	return (p);
+}
