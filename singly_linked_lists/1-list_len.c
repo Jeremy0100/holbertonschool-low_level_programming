d73e4dc0630d4109cb5aabac6f80e0dd90e0872e
Jeremy0100 <jeremy.strady@mailfence.com>
2024-12-24 10:53:15 +0000

    new file:   1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - list number of elements
+ * @h: pointer to a linked list
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	int len = 0;
+
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
