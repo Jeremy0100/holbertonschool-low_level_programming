f66dc58ec72fd2af80f884384d2347c64fe3f20b
Jeremy0100 <jeremy.strady@mailfence.com>
2024-12-24 10:52:23 +0000

    new file:   0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * print_list - print all elements
+ * @h: pointer to list_t
+ * Return: number of elements
+ */
+size_t print_list(const list_t *h)
+{
+	int len = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", h->len, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+
+		h = h->next;
+		len++;
+	}
+
+	return (len);
+}
