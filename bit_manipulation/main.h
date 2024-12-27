746c43e57809a7ed000cc38abf503b22c855b66d
Jeremy0100 <jeremy.strady@mailfence.com>
2024-12-27 14:23:16 +0000

    new file:   0-binary_to_uint.c 	new file:   main.h

---
+++ bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef _lists_h
+#define _lists_h
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+int _pow_recursion(int x, int y);
+int _strlen(const char *s);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+
+#endif
