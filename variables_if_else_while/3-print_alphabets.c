b541264102d30bf2fd3220fc623626b703b52446
Jeremy0100 <jeremy.strady@mailfence.com>
2025-02-19 11:44:59 +0000

    Replaced spaces by tabulations.

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -4,24 +4,24 @@
 * main - Entry point
 *
 * Description : 'Prints the alphabet in lowercase, then
-*               prints the alphabet in uppercase.'
+*		prints the alphabet in uppercase.'
 *
 * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-        int i;
+	int i;
 
-        for (i = 97; i <= 122; i++)
-        {
-        putchar(i);
-        }
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
 
-        for (i = 65; i <= 90; i++)
-        {
-        putchar(i);
-        }
-        putchar('\n');
-        return (0);
+	for (i = 65; i <= 90; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
