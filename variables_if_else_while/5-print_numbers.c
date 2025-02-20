b02a88d73ebc984c6713bc59941003b879806f71
Jeremy0100 <jeremy.strady@mailfence.com>
2025-02-20 14:03:37 +0000

    Print all digit numbers of base 10.

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description : 'Prints all digit numbers of base 10 from 0.'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int number;
+
+for (number = 0; number < 10; number++)
+{
+printf("%d", number);
+}
+
+putchar('\n');
+return (0);
+}
