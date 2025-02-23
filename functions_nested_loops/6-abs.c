a0c2b58acc6b1f6f2f3124a5175c9857a7daa6be
Jeremy0100 <jeremy.strady@mailfence.com>
2025-02-23 15:30:12 +0000

    Script that computes the abosolute value of an integer.

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _abs - computes absolute value of an integer
+*
+* Description: Program with a prototype. Computes
+*		absolute value of an integer.
+*
+* @n: n variable
+*
+* Return: Nothing
+*
+*/
+
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (-n);
+}
