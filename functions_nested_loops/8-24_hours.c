5218e17dc17c56b0f6795f0b84019d5c2d6a6ff7
Jeremy0100 <jeremy.strady@mailfence.com>
2025-02-23 17:05:50 +0000

    Start from 00:00 and finish to 23:59

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* jack_bauer - start from 00:00 to 23:59
+*
+* Description: Prints every minute of the day of Jack Bauer.
+*
+* Return: 1 or 0
+*
+*/
+
+void jack_bauer(void)
+{
+	char h1, h2, m1, m2;
+
+	for (h1 = '0' ; h1 <= '2'; h1++)
+	{
+		for (h2 = '0' ; ((h1 < '2' && h2 <= '9') || (h1 == '2' && h2 <= '3')); h2++)
+		{
+			for (m1 = '0' ; m1 <= '5'; m1++)
+			{
+				for (m2 = '0' ; m2 <= '9'; m2++)
+				{
+					_putchar(h1);
+					_putchar(h2);
+					_putchar(':');
+					_putchar(m1);
+					_putchar(m2);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
