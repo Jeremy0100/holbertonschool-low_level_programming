7000ba293399e1b596916e8a8052efa3410d0c53
Jeremy0100 <jeremy.strady@mailfence.com>
2025-03-07 11:51:34 +0000

    Puts with recursion, prints a string.

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - Puts with recursion, prints a string
+ *			followed by new line.
+ * @s: pointer char
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		putchar('\n');
+	}
+	else
+	{
+		if (*s != '\0')
+		{
+			putchar(*s);
+			_puts_recursion(s + 1);
+		}
+	}
+}
