199a4e75d6f684d60bd9ef45828c1d7b2d87684a
Jeremy0100 <jeremy.strady@mailfence.com>
2025-03-07 18:51:34 +0000

    added _putchar.

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -11,13 +11,13 @@ void _puts_recursion(char *s)
 
 	if (*s == '\0')
 	{
-		putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
 		if (*s != '\0')
 		{
-			putchar(*s);
+			_putchar(*s);
 			_puts_recursion(s + 1);
 		}
 	}
