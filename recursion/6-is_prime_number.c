1283eda959a24d90dff4ed58bca7aec4246f86ff
Jeremy0100 <jeremy.strady@mailfence.com>
2025-03-09 12:36:50 +0000

    A function that reuturns 1 if the input integer is a prime number.

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+>#include "main.h"
+
+/**
+ * isprime - prime number checker
+ * @i: int
+ * @j: int
+ * Return: 1 or 0
+ */
+
+int isprime(int i, int j)
+{
+	if (j < 2 || j % i == 0)
+	{
+		return (0);
+	}
+
+	if (i > (j / 2))
+	{
+		return (1);
+	}
+
+	return (isprime(i + 1, j));
+}
+
+/**
+ * is_prime_number - prime number checker
+ * @n: int
+ * Return: 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 2)
+	{
+		return (1);
+	}
+
+	return (isprime(2, n));
+}
+
