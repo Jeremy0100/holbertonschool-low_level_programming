0c2b3fbcf6d78394c89f18736145261c9659e070
Jeremy0100 <jeremy.strady@mailfence.com>
2025-03-11 08:44:08 +0000

    !

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strdup - copy string
+ * @str: char pointer
+ * Return: char pointer
+ */
+
+char *_strdup(char *str)
+{
+	if (str != NULL)
+	{
+		char *p;
+
+		unsigned long s;
+
+		int i;
+
+		s = strlen(str);
+		p = (char *)malloc(sizeof(char) * s + 1);
+		if (p != NULL)
+		{
+			i = 0;
+			while (str[i])
+			{
+				p[i] = str[i];
+				i++;
+			}
+
+			return (p);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+	else
+	{
+
+		return (NULL);
+	}
+}
