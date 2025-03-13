f36eae7daf970585ac3d286580800a7cf91a7eec
Jeremy0100 <jeremy.strady@mailfence.com>
2025-03-13 20:54:24 +0000

    Free the memory of dog.

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+/**
+ * free_dog - freeing dog struct
+ * @d: dog struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+
+		free(d->name);
+
+		free(d);
+	}
+	else
+	{
+		return;
+	}
+}
