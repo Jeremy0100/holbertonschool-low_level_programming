efd559f724bbef3f806032112e47d0fdbca9b61e
Jeremy0100 <jeremy.strady@mailfence.com>
2025-03-14 22:55:21 +0000

    Doing something completely different.

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,22 +2,19 @@
 #define dog_h
 #include <stdlib.h>
 #include <stdio.h>
-/**
- * struct dog - dog data
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
- */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif
 
+/**
+ * main - dog my_dog
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	struct dog my_dog;
+
+	my_dog.name = "Poppy";
+ 	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	return (0);
+}
