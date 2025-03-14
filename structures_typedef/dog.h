87a3f4de536ed585e59a7826e5f7184dad6ebb45
Jeremy0100 <jeremy.strady@mailfence.com>
2025-03-14 23:08:06 +0000

    Define the new type struct dog with the name, age, and owner.

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,19 +1,23 @@
+#ifndef dog_h
+#define dog_h
+#include <stdlib.h>
 #include <stdio.h>
-#include "dog.h"
-
 /**
- * main - dog my_dog
- * Return: Always 0
+ * struct dog - dog data
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  */
-int main(void)
+struct dog
 {
-
-	struct dog my_dog;
-
-	my_dog.name = "Poppy";
- 	my_dog.age = 3.5;
-	my_dog.owner = "Bob";
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-	return (0);
-}
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
+
